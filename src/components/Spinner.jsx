fae6fb058c1b94c53bc8fce3b7a3299feff226a5
saulbarrios <barriossaul@gmail.com>
2022-06-17 21:51:19 -0500

    Obtener Clima desde API con Context Done

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+const Spinner = () => {
+  return (
+    <div className="loading">
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
